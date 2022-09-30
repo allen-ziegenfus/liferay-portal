4254de4cdbf7a1b5861977a22b1f20f698cf5aa3
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-09-29 20:56:30 -0500

    test params

--- modules/apps/site-initializer/site-initializer-liferay-marketplace/extra/remote-app/src/common/services/liferay/api.js
+++ modules/apps/site-initializer/site-initializer-liferay-marketplace/extra/remote-app/src/common/services/liferay/api.js
@@ -20,6 +20,7 @@ const {REACT_APP_LIFERAY_API = window.location.origin} = process.env;
 export const axios = _axios.create({
 	baseURL: REACT_APP_LIFERAY_API,
 	headers: {
+		'Authorization': 'Basic ' + btoa('liferay@liferay.com:test1'),
 		'x-csrf-token': Liferay.authToken,
 	},
 });
