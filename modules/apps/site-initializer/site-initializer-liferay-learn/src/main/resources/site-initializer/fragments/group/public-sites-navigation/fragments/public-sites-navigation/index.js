7a05ed57691fe1805189a7e52283f45da89f8ec6
Ethan Liao <marineethan@gmail.com>
2022-10-08 14:15:24 -0700

    ISSITES-5743 add dropdown logic and additional styles

--- modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
@@ -11,3 +11,21 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
+var menuButtonGroup = fragmentElement.querySelector('.menu-button-group');
+
+var tabletMobileNavSection = fragmentElement.querySelector('.tablet-mobile-nav-section');
+
+var menuBtn = fragmentElement.querySelector('.menu-btn');
+
+var closeBtn = fragmentElement.querySelector('.close-btn');
+
+menuBtn.addEventListener('click', function() {
+	menuButtonGroup.classList.toggle('menu-open');
+	tabletMobileNavSection.classList.toggle('menu-open');
+});
+
+closeBtn.addEventListener('click', function() {
+	menuButtonGroup.classList.toggle('menu-open');
+	tabletMobileNavSection.classList.toggle('menu-open');
+});
