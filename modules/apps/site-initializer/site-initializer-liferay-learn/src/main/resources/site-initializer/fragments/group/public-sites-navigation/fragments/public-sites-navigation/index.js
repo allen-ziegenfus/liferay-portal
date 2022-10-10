a3418ae6ca833f51687262e59f1864e7647402ac
Ethan Liao <marineethan@gmail.com>
2022-10-10 14:36:02 -0700

    ISSITES-5744 Added functionality to open and close account dropdown

--- modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
@@ -20,6 +20,10 @@ var menuBtn = fragmentElement.querySelector('.menu-btn');
 
 var closeBtn = fragmentElement.querySelector('.close-btn');
 
+const accountMenu = fragmentElement.querySelector('.account-info');
+
+const dropdownContent = fragmentElement.querySelector('.dropdown-content');
+
 menuBtn.addEventListener('click', function() {
 	menuButtonGroup.classList.toggle('menu-open');
 	tabletMobileNavSection.classList.toggle('menu-open');
@@ -29,3 +33,11 @@ closeBtn.addEventListener('click', function() {
 	menuButtonGroup.classList.toggle('menu-open');
 	tabletMobileNavSection.classList.toggle('menu-open');
 });
+
+function showAccountDropdown() {
+	dropdownContent.classList.toggle('show');
+	dropdownContent.classList.toggle('text-primary');
+	accountMenu.classList.toggle('text-primary');
+}
+
+accountMenu.addEventListener('click', showAccountDropdown);
