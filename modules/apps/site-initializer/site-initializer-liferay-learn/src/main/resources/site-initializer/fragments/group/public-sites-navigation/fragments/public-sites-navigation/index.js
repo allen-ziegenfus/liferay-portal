28b8e7b8ebb1ac809006c8290bd8b85c92b5b352
Ethan Liao <marineethan@gmail.com>
2022-10-12 11:27:14 -0700

    ISSITES-5744 Added functionality and styling for the account menu to appear in the primary nav when sign in cta is present according to mockups

--- modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
@@ -14,30 +14,28 @@
 
 var menuButtonGroup = fragmentElement.querySelector('.menu-button-group');
 
-var tabletMobileNavSection = fragmentElement.querySelector('.tablet-mobile-nav-section');
+var tabletMobileNavSection = fragmentElement.querySelector(
+	'.tablet-mobile-nav-section'
+);
 
 var menuBtn = fragmentElement.querySelector('.menu-btn');
 
 var closeBtn = fragmentElement.querySelector('.close-btn');
 
-const accountMenu = fragmentElement.querySelector('.account-info');
+var accountMenus = fragmentElement.querySelectorAll('.account');
 
-const dropdownContent = fragmentElement.querySelector('.dropdown-content');
-
-menuBtn.addEventListener('click', function() {
+menuBtn.addEventListener('click', function () {
 	menuButtonGroup.classList.toggle('menu-open');
 	tabletMobileNavSection.classList.toggle('menu-open');
 });
 
-closeBtn.addEventListener('click', function() {
+closeBtn.addEventListener('click', function () {
 	menuButtonGroup.classList.toggle('menu-open');
 	tabletMobileNavSection.classList.toggle('menu-open');
 });
 
-function showAccountDropdown() {
-	dropdownContent.classList.toggle('show');
-	dropdownContent.classList.toggle('text-primary');
-	accountMenu.classList.toggle('text-primary');
-}
-
-accountMenu.addEventListener('click', showAccountDropdown);
+accountMenus.forEach(function (accountMenu) {
+	accountMenu.addEventListener('click', function () {
+		accountMenu.classList.toggle('menu-open');
+	});
+});
