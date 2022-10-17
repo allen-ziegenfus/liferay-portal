373edea3b51f9caac577bb3b152a9d666670724d
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-10-16 22:51:17 -0500

    Add become a publisher app

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/become-a-publisher-remote-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  return (
+    <div className='become-a-publisher-container'>
+      <h1>Become a Publisher</h1>
+    </div>
+  );
+}
+
+export default App;
