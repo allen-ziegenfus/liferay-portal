2a0826e66d95ffaefc707103fbfe16fa0bfb8ccf
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-10-18 14:38:50 -0500

    Add eslint

--- workspaces/liferay-marketplace-workspace/client-extensions/dashboard-remote-app/src/App.js
+++ workspaces/liferay-marketplace-workspace/client-extensions/dashboard-remote-app/src/App.js
@@ -1,4 +1,7 @@
 import logo from './logo.svg';
+
+
+
 import './App.css';
 
 function App() {
@@ -7,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save this to reload.
         </p>
         <a
           className="App-link"
