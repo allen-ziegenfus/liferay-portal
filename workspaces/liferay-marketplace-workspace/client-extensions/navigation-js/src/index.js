612862a46389653eedf512337519810ad8345e35
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2022-12-01 17:37:43 -0600

    ISSITES-5784 Add navigation-js extension

---
+++ workspaces/liferay-marketplace-workspace/client-extensions/navigation-js/src/index.js
@@ -0,0 +1,9 @@
+import DropdownProvider from './DropdownProvider';
+import ClassToggle from './ClassToggle';
+import SpatialNavigationProvider from './SpatialNavigation';
+
+export default {
+	ClassToggle,
+	DropdownProvider,
+	SpatialNavigationProvider
+};
