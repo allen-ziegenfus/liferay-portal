69331e28c652196a73c61dd094ad553c584e0913
Johanna Tchon <johanna.tchon@liferay.com>
2023-01-13 18:49:05 -0800

    ISSITES-5888 SF

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/webpack.config.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/webpack.config.js
@@ -2,28 +2,28 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
-    module: {
-        rules: [
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
-              }
-            },          
-            {
-              test: /\.css$/,
-              use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-            }
-        ]
-    },
-    output: {
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				exclude: /node_modules/,
+				test: /\.js$/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+			}
+		]
+	},
+	output: {
 		filename: 'index.js',
 		globalObject: 'this',
 		path: path.resolve(__dirname, 'build/static')
-	},
-    mode: 'development'
+	}
 };
