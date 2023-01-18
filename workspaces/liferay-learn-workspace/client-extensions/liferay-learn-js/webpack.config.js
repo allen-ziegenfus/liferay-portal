a75a9efc680031397bd63f40b5700016b84da370
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-01-17 20:46:24 -0600

    ISSITES-5888 SF

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/webpack.config.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/webpack.config.js
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 const path = require('path');
 
 module.exports = {
@@ -11,19 +25,19 @@ module.exports = {
 				use: {
 					loader: 'babel-loader',
 					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
+						presets: ['@babel/preset-env'],
+					},
+				},
 			},
 			{
 				test: /\.css$/,
-				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-			}
-		]
+				use: [{loader: 'style-loader'}, {loader: 'css-loader'}],
+			},
+		],
 	},
 	output: {
 		filename: 'index.js',
 		globalObject: 'this',
-		path: path.resolve(__dirname, 'build/static')
-	}
+		path: path.resolve(__dirname, 'build/static'),
+	},
 };
