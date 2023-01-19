e6a31cfc6b4d94b5bf121cbf8a118123cff24b37
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-01-18 23:54:05 -0600

    ISSITES-5914 SF

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/src/article.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/src/article.js
@@ -1,10 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 function initArticle() {
+
 	// Table of contents reading indicator
 
 	const headings = document.querySelectorAll('.article-body h2');
 
 	let activeIndex;
-	let targets = [];
+	const targets = [];
 
 	if (headings) {
 		const articleTOC = document.getElementById('articleTOC');
@@ -13,32 +28,32 @@ function initArticle() {
 			articleTOC.innerHTML = '';
 		}
 
-		headings.forEach(
-			heading => {
-				const id = heading.querySelector('a').hash.replace('#', '');
+		headings.forEach((heading) => {
+			const id = heading.querySelector('a').hash.replace('#', '');
 
-				if (articleTOC) {
-					articleTOC.innerHTML += `
+			if (articleTOC) {
+				articleTOC.innerHTML += `
 				<li class="nav-item">
 					<a class="nav-link" href="#${id}" id="toc-${id}">
 						${heading.innerText}
 					</a>
 				</li>`;
-				}
-
-				targets.push({ id: id, isIntersecting: false });
 			}
-		);
+
+			targets.push({id, isIntersecting: false});
+		});
 	}
 
-	const callback = entries => {
-		entries.forEach(entry => {
-			const index = targets.findIndex(target => target.id === entry.target.id);
+	const callback = (entries) => {
+		entries.forEach((entry) => {
+			const index = targets.findIndex(
+				(target) => target.id === entry.target.id
+			);
 
 			targets[index].isIntersecting = entry.isIntersecting;
 
 			if (!targets[activeIndex] || !targets[activeIndex].isIntersecting) {
-				setActiveIndex()
+				setActiveIndex();
 			}
 		});
 
@@ -49,14 +64,19 @@ function initArticle() {
 
 	// rootMargin of 157px is header height + info bar height + 24px gutter offset
 
-	const observer = new IntersectionObserver(callback, { rootMargin: '-157px', threshold: [0, 0.2, 0.4, 0.6, 0.8, 1] });
+	const observer = new IntersectionObserver(callback, {
+		rootMargin: '-157px',
+		threshold: [0, 0.2, 0.4, 0.6, 0.8, 1],
+	});
 
 	const setActiveIndex = () => {
-		activeIndex = targets.findIndex(target => target.isIntersecting === true);
+		activeIndex = targets.findIndex(
+			(target) => target.isIntersecting === true
+		);
 	};
 
-	const toggleActiveClass = id => {
-		targets.forEach(target => {
+	const toggleActiveClass = (id) => {
+		targets.forEach((target) => {
 			const node = document.getElementById(`toc-${target.id}`);
 
 			if (node) {
@@ -69,23 +89,26 @@ function initArticle() {
 		if (activeNode) {
 			activeNode.classList.add('active');
 		}
-	}
+	};
 
-	targets.forEach(target => {
+	targets.forEach((target) => {
 		const node = target.id ? document.getElementById(target.id) : null;
 
 		if (node) {
 			observer.observe(node);
 
-			node.style.cssText = "margin-top: -157px; padding-top: 157px;"
+			node.style.cssText = 'margin-top: -157px; padding-top: 157px;';
 		}
 	});
 
-	const productDocumentationSelector = document.getElementById('productDocumentationSelector');
+	const productDocumentationSelector = document.getElementById(
+		'productDocumentationSelector'
+	);
 
 	if (productDocumentationSelector) {
-		productDocumentationSelector.addEventListener('change', event => {
-			var selectedOption = event.target.options[event.target.selectedIndex];
+		productDocumentationSelector.addEventListener('change', (event) => {
+			const selectedOption =
+				event.target.options[event.target.selectedIndex];
 
 			window.location.pathname = selectedOption.dataset.href;
 		});
@@ -99,7 +122,7 @@ function initArticle() {
 	if (docNavWrapper && mobileDocNavToggler) {
 		const togglers = mobileDocNavToggler.querySelectorAll('button');
 
-		togglers.forEach(toggler =>
+		togglers.forEach((toggler) =>
 			toggler.addEventListener('click', () => {
 				docNavWrapper.classList.toggle('mobile-nav-hide');
 			})
