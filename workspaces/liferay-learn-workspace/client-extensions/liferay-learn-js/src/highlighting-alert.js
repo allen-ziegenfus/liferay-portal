882760cb4dbe02f46b3dfa743259ca8b6784a406
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-01-18 22:49:55 -0600

    ISSITES-5672 Fix highlighting

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/src/highlighting-alert.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-js/src/highlighting-alert.js
@@ -1,3 +1,5 @@
+import Mark from 'mark.js';
+
 /**
 * Adds an alert with the text being highlighted and provides a way to remove
 * the highlight styling.
@@ -11,14 +13,6 @@ const HIGHLIGHT_PARAM = 'highlight';
 
 const TRUNCATE_LENGTH = 50;
 
-/**
-* Calls Sphinx utility to remove highlighted text.
-* https://github.com/sphinx-doc/sphinx/blob/9e1b4a8f1678e26670d34765e74edf3a3be3c62c/sphinx/themes/basic/static/doctools.js#L261-L267
-*/
-function hideSearchWords() {
-	Documentation.hideSearchWords(); // eslint-disable-line
-}
-
 function initHighlightingAlert() {
 	const urlSearchParams = new URLSearchParams(window.location.search);
 
@@ -29,46 +23,52 @@ function initHighlightingAlert() {
 
 		if (highlightAlertElement) {
 
-			// Add text being highlighted
+			const articleBody = document.querySelector('.article-body');
+			if (articleBody) {
+				var mark = new Mark(articleBody);
 
-			const textMatchElement = document.getElementById(
-				HIGHLIGHT_TEXT_MATCH_ID
-			);
+				// Add text being highlighted
 
-			if (textMatchElement) {
-				let searchTerm = urlSearchParams.get(HIGHLIGHT_PARAM);
+				const textMatchElement = document.getElementById(
+					HIGHLIGHT_TEXT_MATCH_ID
+				);
 
-				if (searchTerm.length > TRUNCATE_LENGTH) {
-					searchTerm =
-						urlSearchParams
-							.get(HIGHLIGHT_PARAM)
-							.slice(0, TRUNCATE_LENGTH) + '...';
+				if (textMatchElement) {
+					let searchTerm = urlSearchParams.get(HIGHLIGHT_PARAM);
+
+					if (searchTerm.length > TRUNCATE_LENGTH) {
+						searchTerm =
+							urlSearchParams
+								.get(HIGHLIGHT_PARAM)
+								.slice(0, TRUNCATE_LENGTH) + '...';
+					}
+
+					textMatchElement.textContent = ' "' + searchTerm + '"';
+					textMatchElement.title = urlSearchParams.get(HIGHLIGHT_PARAM);
+
+					mark.mark(searchTerm, {'accuracy': 'exactly', 'className': 'highlighted'});
 				}
 
-				textMatchElement.textContent = ' "' + searchTerm + '"';
-				textMatchElement.title = urlSearchParams.get(HIGHLIGHT_PARAM);
-			}
+				// Setup remove highlight link to clear highlights and dismiss alert
 
-			// Setup remove highlight link to clear highlights and dismiss alert
-
-			const removeHighlightLinkElement = document.getElementById(
-				REMOVE_HIGHLIGHT_LINK_ID
-			);
-
-			if (removeHighlightLinkElement) {
-				removeHighlightLinkElement.addEventListener(
-					'click',
-					function () {
-						hideSearchWords();
-
-						highlightAlertElement.remove();
-					}
+				const removeHighlightLinkElement = document.getElementById(
+					REMOVE_HIGHLIGHT_LINK_ID
 				);
+
+				if (removeHighlightLinkElement) {
+					removeHighlightLinkElement.addEventListener(
+						'click',
+						function () {
+							mark.unmark();
+							highlightAlertElement.remove();
+						}
+					);
+				}
+
+				// Show alert
+
+				highlightAlertElement.classList.remove('hide');
 			}
-
-			// Show alert
-
-			highlightAlertElement.classList.remove('hide');
 		}
 	}
 }
