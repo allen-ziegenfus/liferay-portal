38329d1661f660b1b1dc0a54261fbcbac1f370cb
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-01-21 08:49:08 -0600

    ISSITES-5878 Add hash to navigation js file build

--- workspaces/liferay-dot-com-workspace/client-extensions/navigation-js/webpack.config.js
+++ workspaces/liferay-dot-com-workspace/client-extensions/navigation-js/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
 	entry: './src/index.js',
 	output: {
-		filename: 'index.js',
+		filename: '[name]-[hash].js',
 		globalObject: 'this',
 		library: {
 			name: 'navigation',
