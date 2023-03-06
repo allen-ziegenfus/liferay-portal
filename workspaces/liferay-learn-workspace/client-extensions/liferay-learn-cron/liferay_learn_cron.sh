51275b05b380cbfd4cbcec5f2967d96fad3cc011
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-03-06 17:33:32 -0600

    ISSITES-5302 Base dockerfile on jdk11-jdk8

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-cron/liferay_learn_cron.sh
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-cron/liferay_learn_cron.sh
@@ -171,7 +171,7 @@ if [ -z "$LIFERAY_LEARN_CRON_LIFERAY_URL" ] ; then
 	LIFERAY_LEARN_CRON_LIFERAY_URL=https://$(cat /etc/liferay/lxc/dxp-metadata/com.liferay.lxc.dxp.mainDomain)
 fi
 
-java -Xmx2048m -jar /opt/liferay/liferay-learn-cron.jar
+java -Xmx2048m -jar /liferay-learn-cron.jar
 
 IMPORT_RC=$?
 
