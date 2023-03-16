e0af79f6e4a55765927ccf39d9584a13f8521f3e
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-03-16 11:52:56 -0500

    ISSITES-5302 squash everything

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron/test-importer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export LIFERAY_DATA_DEFINITION_KEY=LEARN-ARTICLE
+export LIFERAY_GROUP_FRIENDLY_URL_PATH=guest
+export LIFERAY_OAUTH_CLIENT_ID=
+export LIFERAY_OAUTH_CLIENT_SECRET=
+export LIFERAY_URL=https://dxp.lfr.dev
+export MARKDOWN_IMPORT_DIR=/home/allenz/liferay/liferay-learn-test-dxp
+export OFFLINE=true
+
+blade gw run
