1ca762dbaa2698bf26e3c5080d188a94104a99f7
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2023-04-25 22:41:24 -0500

    LPS-181171 SF

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-fds-cell-renderer/src/index.tsx
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-fds-cell-renderer/src/index.tsx
@@ -1,12 +1,12 @@
-import type {FDSCellRenderer} from '@liferay/js-api/data-set';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import type {FDSCellRenderer} from '@liferay/js-api/data-set';
 
 const fdsCellRenderer: FDSCellRenderer = ({value}) => {
 	const element = document.createElement('div');
 
-	ReactDOM.render(<>{value == 'Green' ? '🍏' : value}</>, element);
+	ReactDOM.render(<>{value === 'Green' ? '🍏' : value}</>, element);
 
 	return element;
 };
