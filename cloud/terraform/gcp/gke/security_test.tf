f6ad502eda2ea97f02fa4c0b52ca507a286f2a72
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-03-16 23:12:39 -0500

    update

--- cloud/terraform/gcp/gke/security_test.tf
+++ cloud/terraform/gcp/gke/security_test.tf
@@ -1,5 +1,5 @@
 locals {
-	dummy_secret="AKIAIMssNssOssssssJsassVYsEsXsssAKsEY"
+	dummy_secret="AKIAIMssNssOsssssssJsassVYsEsXsssAKsEY"
 }
 resource "google_compute_firewall" "insecure_rule" {
 	allow {
