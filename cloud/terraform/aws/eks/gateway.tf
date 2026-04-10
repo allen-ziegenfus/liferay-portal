dbcb6205ab01ed9f25026a8fad566c78f7f6dbc6
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2026-04-09 19:01:36 -0500

    LCD-50854 Add PDB for envoy proxy

--- cloud/terraform/aws/eks/gateway.tf
+++ cloud/terraform/aws/eks/gateway.tf
@@ -31,8 +31,20 @@ resource "helm_release" "envoy_gateway" {
 							enableBackend=false
 						}
 					}
+					envoyProxy={
+						deployment={
+							pdb={
+								enabled=true
+								minAvailable=1
+							}
+						}
+					}
 				}
 				deployment={
+					pdb={
+						enabled=true
+						minAvailable=1
+					}
 					replicas=2
 				}
 			}),
